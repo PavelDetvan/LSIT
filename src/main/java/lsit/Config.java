496a8d8baa7ed2a0a83a67f537547a2a2d479b98
martinwss1337 <martinpopov1337@gmail.com>
2024-11-30 23:49:42 +0100

    yamal

---
+++ src/main/java/lsit/Config.java
@@ -0,0 +1,26 @@
+package lsit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(c -> c.disable())
+            .oauth2Login(withDefaults())
+            .authorizeHttpRequests(a -> a
+                .requestMatchers("/user").authenticated()
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
--- src/main/java/lsit/Controllers/HomeController.java
+++ src/main/java/lsit/Controllers/HomeController.java
@@ -1,6 +1,7 @@
 package lsit.Controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,18 @@ public class HomeController {
     @GetMapping("/")
     public ResponseEntity get(){
         return ResponseEntity.ok("Welcome to the Car Rental Service!");
+    }
+
+    @GetMapping("/user")
+    public String getUser(OAuth2AuthenticationToken authentication){
+        var userAttributes = authentication.getPrincipal().getAttributes();
+
+        return "<pre>\n" +
+            userAttributes.entrySet().parallelStream().collect(
+                StringBuilder::new,
+                (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()).append("\n"),
+                StringBuilder::append
+            ).toString() +
+            "</pre>";
     }
 }
